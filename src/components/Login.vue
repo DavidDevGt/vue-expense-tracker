0d90dd1b8e2d6addc5b80ea52c8b242f26453936
DavidDevGt <josuedavidvl18@gmail.com>
2024-02-06 00:25:24 -0600

    nuevos componentes para el login

---
+++ src/components/Login.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="login-container">
+        
+    </div>
+</template>
+
+<script setup>
+import AuthService from '@/services/AuthService';
+import { useToast } from 'vue-toastification';
+import { ref } from 'vue';
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Logout.vue
@@ -0,0 +1,10 @@
+<template>
+  <button @click="logout">Cerrar sesión</button>
+</template>
+
+<script>
+import AuthService from '@/services/AuthService';
+import { useToast } from 'vue-toastification';
+
+
+</script>
